36f5d79df05d7c09a419627e3251b75040e29f8e
Almir Motika <Almir_18B@hotmail.com>
2024-02-13 23:09:58 +0100

    12. update product service

--- src/app/component/product-list/product-list.component.ts
+++ src/app/component/product-list/product-list.component.ts
@@ -18,12 +18,14 @@ export class ProductListComponent implements OnInit {
     private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(()=>{
     this.listProducts();
+    });
   }
   listProducts() {
     const hasCategoryId:boolean=this.route.snapshot.paramMap.has('id');
     if(hasCategoryId){
-      this.currentCategoryId=+this.route.snapshot.paramMap.get('if')!;
+      this.currentCategoryId=+this.route.snapshot.paramMap.get('id')!;
     }
     else{
       this.currentCategoryId=1;
