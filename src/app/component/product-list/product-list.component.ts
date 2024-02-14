ce051b55c5afc7dd9d8e15ed969c8f3520058018
Almir Motika <Almir_18B@hotmail.com>
2024-02-14 16:45:06 +0100

    13. create product-category and menu component,product service call URL on spring boot

--- src/app/component/product-list/product-list.component.ts
+++ src/app/component/product-list/product-list.component.ts
@@ -14,6 +14,7 @@ export class ProductListComponent implements OnInit {
 
   products:Product[]=[];
   currentCategoryId: number=1;
+  currentCategoryName: string ="";
   constructor(private productService: ProductService,
     private route:ActivatedRoute) { }
 
@@ -26,9 +27,11 @@ export class ProductListComponent implements OnInit {
     const hasCategoryId:boolean=this.route.snapshot.paramMap.has('id');
     if(hasCategoryId){
       this.currentCategoryId=+this.route.snapshot.paramMap.get('id')!;
+      this.currentCategoryName=this.route.snapshot.paramMap.get('name')!;
     }
     else{
       this.currentCategoryId=1;
+      this.currentCategoryName='Books';
     }
     this.productService.getProductList(this.currentCategoryId).subscribe(
       data=>{this.products=data;}
