ef7d344ddc77e41da9453ef6e2b7f0328c62551b
Almir Motika <Almir_18B@hotmail.com>
2024-02-14 21:22:11 +0100

    15. enhance productListComponent, update product service api,handle for no products found

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -9,20 +9,29 @@ import { ProductCategory } from '../common/product-category';
 })
 export class ProductService {
 
+
 private baseUrl='http://localhost:8080/api/products';
 private categoryUrl='http://localhost:8080/api/product-category';
   constructor(private httpClient:HttpClient) { }
 
   getProductList(theCategoryId:number): Observable<Product[]>{
     const searchUrl=`${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    return this.getProducts(searchUrl);
+  }
+  private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map(response=>response._embedded.products)
+      map(response => response._embedded.products)
     );
   }
+
   getProductCategories():Observable<ProductCategory[]> {
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response=>response._embedded.productCategory)
     );
+  }
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    const searchUrl=`${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+    return this.getProducts(searchUrl);
   }
 }
 interface GetResponseProducts{
