4786492d8c4afe37c422791fbec69f1581d892a8
Almir Motika <Almir_18B@hotmail.com>
2024-02-29 21:28:06 +0100

    22. checkout form validation

---
+++ src/app/validators/amshop-validators.spec.ts
@@ -0,0 +1,7 @@
+import { AMShopValidators } from './amshop-validators';
+
+describe('AMShopValidators', () => {
+  it('should create an instance', () => {
+    expect(new AMShopValidators()).toBeTruthy();
+  });
+});
