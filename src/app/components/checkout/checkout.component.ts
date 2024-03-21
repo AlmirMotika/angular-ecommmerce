1e2fa4b1d90fa878aeb1da85bc32c97b01a6c9a1
Almir Motika <Almir_18B@hotmail.com>
2024-03-21 13:49:43 +0100

    28. security - order history - base functionality

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -32,6 +32,8 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates:State[]=[];
   billingAddressStates:State[]=[];
 
+  storage:Storage=sessionStorage;
+
   constructor(private formBuilder:FormBuilder,
               private amShopFormService:AMShopFormService,
               private cartService:CartService,
@@ -41,6 +43,8 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
 
     this.reviewCartDetails();
+
+    const theEmail=JSON.parse(this.storage.getItem('userEmail')!);
 
     this.checkoutFormGroup=this.formBuilder.group({
       customer:this.formBuilder.group({
@@ -54,7 +58,7 @@ export class CheckoutComponent implements OnInit {
                                Validators.minLength(2),
                                AMShopValidators.notOnlyWhitespace]),
 
-        email:new FormControl('',
+        email:new FormControl(theEmail,
                              [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
